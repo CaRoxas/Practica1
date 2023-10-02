361d39caac3e2ff6fe43d5e778a78a03372b7f2e
CaRoxas <carlotacarballolol@gmail.com>
2023-10-02 17:05:51 +0100

    Dia 02/10
    Movimiento del machango y la rotacion

---
+++ Assets/Scenes/Jugador.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public float movX;
+    public float movY;
+    public float tiempo;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo = tiempo + Time.deltaTime;
+        Debug.Log(tiempo);
+        movX = Input.GetAxis("Horizontal");
+        movY = Input.GetAxis("Vertical");
+        Vector2 direccion = new Vector2(movX, movY);
+        rb.velocity = direccion * 3;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Paredes")
+        {
+            PauseGame();
+        }
+
+    }
+
+    void PauseGame ()
+    {
+        Time.timeScale = 0;
+    }
+    void ResumeGame ()
+    {
+        Time.timeScale = 1;
+    }
+}
