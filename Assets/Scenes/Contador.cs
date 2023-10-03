652f5c6b61e7a50b3b76637ef7607d93bd535aa2
CaRoxas <carlotacarballolol@gmail.com>
2023-10-03 16:06:29 +0100

    Añadir monedas y texto
    Monedas que se desvanezcan y empezar el texto contador

--- Assets/Scenes/Contador.cs
+++ Assets/Scenes/Contador.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
-public class ParedRotativa : MonoBehaviour
+public class Contador : MonoBehaviour
 {
+    public TMP_Text textoContador;
+    float Tiempo = 0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward * 90 * Time.deltaTime);
-
+        textoContador.text = "";
     }
 }
